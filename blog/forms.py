e23b21fcabdbb638052f944a957037fc3306e3dd
Anna Kupś <kups@student.agh.edu.pl>
2015-06-20 15:59:40 +0200

    Fixed indents

--- blog/forms.py
+++ blog/forms.py
@@ -10,7 +10,7 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
-
-	class Meta:
-		model = Comment
-		fields = ('author', 'text',)
+    
+    class Meta:
+	model = Comment
+	fields = ('author', 'text',)
